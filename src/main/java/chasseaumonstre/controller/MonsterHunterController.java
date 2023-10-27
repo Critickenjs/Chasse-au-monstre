49b955e5c2b5ac6c244caded15fa6a8d1d5c1321
yliess el atifi <yliesselatifi@gmail.com>
2023-10-27 11:41:24 +0200

    MonsterHunterController update

--- src/main/java/chasseaumonstre/controller/MonsterHunterController.java
+++ src/main/java/chasseaumonstre/controller/MonsterHunterController.java
@@ -1,5 +1,19 @@
 package chasseaumonstre.controller;
 
+import chasseaumonstre.model.MonsterHunterModel;
+import chasseaumonstre.views.MonsterHunterMenuVue;
+import chasseaumonstre.views.MonsterHunterPartieVue;
+
 public class MonsterHunterController {
+    private MonsterHunterModel model;
+    private MonsterHunterPartieVue partieVue;
+    private MonsterHunterMenuVue menuVue;
+
+    public MonsterHunterController() {
+        model = new MonsterHunterModel();
+        partieVue = new MonsterHunterPartieVue(this);
+        menuVue = new MonsterHunterMenuVue(this);
+        isHuntersTurn = true; 
+    }
     
 }
