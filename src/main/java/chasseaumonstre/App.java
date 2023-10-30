c025b2eb13ccc72e38d7baebaddba9e556050948
yliess el atifi <yliesselatifi@gmail.com>
2023-10-30 14:40:01 +0100

    app update

--- src/main/java/chasseaumonstre/App.java
+++ src/main/java/chasseaumonstre/App.java
@@ -1,7 +1,24 @@
 package chasseaumonstre;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("..\\..\\..\\..\\maquette\\main.fxml")); 
+
+        Scene scene = new Scene(root, 1080, 600); 
+
+        primaryStage.setTitle("Chasse au monstre");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
