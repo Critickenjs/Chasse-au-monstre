7f7220f3a4e7ef997cfa105fbe40469e075394a0
LilFather <indatilla@gmail.com>
2023-10-31 19:32:27 +0100

    update app
    show main menu

--- src/main/java/chasseaumonstre/App.java
+++ src/main/java/chasseaumonstre/App.java
@@ -1,10 +1,8 @@
 package chasseaumonstre;
 
-import chasseaumonstre.controller.MonsterHunterController;
 import chasseaumonstre.controller.MonsterHunterMenuController;
 import chasseaumonstre.model.MonsterHunterModel;
 import chasseaumonstre.views.MonsterHunterMenuVue;
-import chasseaumonstre.views.MonsterHunterPartieVue;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -12,7 +10,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         MonsterHunterModel model = new MonsterHunterModel(21, 17);
-        new MonsterHunterPartieVue(stage, new MonsterHunterController(model));
+        new MonsterHunterMenuVue(stage, new MonsterHunterMenuController(stage, model));
     }
 
     public static void main(String[] args) {
