ebc6c21cccd628474743dff160cb6e77ec92c12c
Yliess Elatifi <yliess.elatifi.etu@univ-lille.fr>
2023-10-31 20:28:24 +0100

    ajout son dans alert

--- src/main/java/chasseaumonstre/controller/MonsterHunterController.java
+++ src/main/java/chasseaumonstre/controller/MonsterHunterController.java
@@ -1,16 +1,22 @@
 package chasseaumonstre.controller;
 
+import java.io.File;
+
 import chasseaumonstre.model.MonsterHunterModel;
 import chasseaumonstre.views.MonsterHunterPartieVue;
 import fr.univlille.iutinfo.cam.player.perception.ICellEvent.CellInfo;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class MonsterHunterController {
     private Stage stage;
     private MonsterHunterModel model;
     private MonsterHunterPartieVue partieView;
+    private String musicFile = "C:\\Users\\ylies\\Desktop\\saé\\J3_SAE3A\\src\\main\\resources\\audio\\Gun shoot - Sound effect.mp3";
+    
 
     public MonsterHunterController(Stage stage, MonsterHunterModel model) {
         this.stage = stage;
@@ -24,6 +30,7 @@ public class MonsterHunterController {
     public void setVue(MonsterHunterPartieVue partieView) {
         this.partieView = partieView;
     }
+
 
     public void handleShot(int shotX, int shotY) {
         CellInfo cellValue = model.getMaze()[shotX][shotY];
@@ -41,12 +48,27 @@ public class MonsterHunterController {
                 break;
         }
     }
+    public void playSound(String musicFile, double volume) {
+    try {
+        Media sound = new Media(new File(musicFile).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(sound);
+
+        mediaPlayer.setVolume(volume); // Ajustez le volume ici 
+
+        mediaPlayer.play();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
 
     private void pathAlert(int cellX, int cellY) {
+        playSound(musicFile,0.5 );
         showAlert("You shot a path cell. Keep searching!\nCoordinates: (" + cellX + ", " + cellY + ").");
+        
     }
 
     private void wallAlert(int cellX, int cellY) {
+        playSound(musicFile,0.5 );
         showAlert("You shot a wall. Keep searching!");
     }
 
