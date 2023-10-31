391579fc0d5706a3578f4caca987492a02fa52ed
LilFather <indatilla@gmail.com>
2023-10-31 19:34:04 +0100

    update MonsterHunterController
    constructor now needs a stage

--- src/main/java/chasseaumonstre/controller/MonsterHunterController.java
+++ src/main/java/chasseaumonstre/controller/MonsterHunterController.java
@@ -2,12 +2,15 @@ package chasseaumonstre.controller;
 
 import chasseaumonstre.model.MonsterHunterModel;
 import chasseaumonstre.views.MonsterHunterPartieVue;
+import javafx.stage.Stage;
 
 public class MonsterHunterController {
+    private Stage stage;
     private MonsterHunterModel model;
     private MonsterHunterPartieVue partieView;
 
-    public MonsterHunterController(MonsterHunterModel model) {
+    public MonsterHunterController(Stage stage, MonsterHunterModel model) {
+        this.stage = stage;
         this.model = model;
     }
 
