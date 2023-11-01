d955b2a4043306c90f7c79d84140e187f3e05ad9
Yliess Elatifi <yliess.elatifi.etu@univ-lille.fr>
2023-11-01 18:14:38 +0100

    icon

--- src/main/java/chasseaumonstre/controller/MHMenuController.java
+++ src/main/java/chasseaumonstre/controller/MHMenuController.java
@@ -60,5 +60,7 @@ public class MHMenuController  {
         // Chargez l'image depuis l'URL et affectez-la à l'ImageView
         Image image = new Image("https://www.premiere.fr/sites/default/files/styles/scale_crop_border_1280x720/public/2020-02/m.jpg");
         imageView.setImage(image);
+        this.stage.getIcons().add(new Image("https://4.bp.blogspot.com/-boWx8QCf9bA/UYrk_pyI0aI/AAAAAAAAAoo/936FQO4QlNQ/s1600/dj.png"));
+
     }
 }
