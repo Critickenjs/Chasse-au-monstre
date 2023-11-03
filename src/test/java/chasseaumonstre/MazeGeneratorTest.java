0fda4af7d4fc15a1a43aefc7cc2c32558c3853a9
Yliess Elatifi <yliess.elatifi.etu@univ-lille.fr>
2023-11-03 16:11:13 +0100

    test MazeGenerator

---
+++ src/test/java/chasseaumonstre/MazeGeneratorTest.java
@@ -0,0 +1,53 @@
+package chasseaumonstre;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import chasseaumonstre.model.MazeGenerator;
+import fr.univlille.iutinfo.cam.player.perception.ICellEvent.CellInfo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MazeGeneratorTest {
+    private MazeGenerator generator;
+
+    @BeforeEach
+    public void setUp() {
+        generator = new MazeGenerator(10, 10);
+        generator.generate();
+    }
+
+    @Test
+    public void testMazeGeneration() {
+        int[][] maze = generator.getMaze();
+
+        assertEquals(10, maze.length);
+        assertEquals(10, maze[0].length);
+
+        assertEquals(0, maze[generator.getEntrance()][0]); // Entrée
+        assertEquals(0, maze[generator.getExit()][9]); // Sortie
+    }
+
+    @Test
+    public void testCellInfoConversion() {
+        CellInfo[][] cellInfo = generator.toCellInfo();
+
+        assertEquals(CellInfo.MONSTER, cellInfo[generator.getEntrance()][0]);
+        assertEquals(CellInfo.EXIT, cellInfo[generator.getExit()][9]);
+        boolean foundEmpty = false;
+        boolean foundWall = false;
+
+        for (int y = 0; y < 10; y++) {
+            for (int x = 0; x < 10; x++) {
+                if (cellInfo[x][y] == CellInfo.WALL) {
+                    foundWall = true;
+                } else if (cellInfo[x][y] == CellInfo.EMPTY) {
+                    foundEmpty = true;
+                }
+            }
+        }
+
+        assertTrue(foundEmpty);
+        assertTrue(foundWall);
+    }
+}
