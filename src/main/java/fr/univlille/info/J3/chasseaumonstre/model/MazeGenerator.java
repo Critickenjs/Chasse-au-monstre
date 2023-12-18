0c6f540cfdc1dc036ace21ade33dcf75bdaed1b1
yliess El Atifi <yliess.elatifi.etu@univ-lille.fr>
2023-12-18 20:12:36 +0100

    ajout de dfs pour la création de chemin need to be fix

--- src/main/java/fr/univlille/info/J3/chasseaumonstre/model/MazeGenerator.java
+++ src/main/java/fr/univlille/info/J3/chasseaumonstre/model/MazeGenerator.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.Stack;
 
 /*
  * MazeGenerator génère un labyrinthe aléatoirement, validé par MazeValidator
@@ -28,6 +29,7 @@ public class MazeGenerator {
      * Constructeur de MazeGenerator
      * 
      * @param width la largeur du labyrinthe
+     * 
      * @param height la hauteur du labyrinthe
      */
     public MazeGenerator(int width, int height) {
@@ -36,6 +38,7 @@ public class MazeGenerator {
         this.maze = new int[width][height];
         this.random = new Random();
     }
+
     public int getWidth() {
         return this.width;
     }
@@ -44,13 +47,13 @@ public class MazeGenerator {
         return this.height;
     }
 
-
     public int[][] getMaze() {
         return this.maze;
     }
 
     /*
-     * Génère un labyrinthe aléatoirement (tableau d'entiers) avec une entrée et une sortie
+     * Génère un labyrinthe aléatoirement (tableau d'entiers) avec une entrée et une
+     * sortie
      * 0 : chemin
      * 1 : mur
      * 2 : monstre (entrée)
@@ -76,31 +79,36 @@ public class MazeGenerator {
     }
 
     /*
-     * Génère un labyrinthe aléatoirement (tableau d'entiers) avec une entrée et une sortie
+     * Génère un labyrinthe aléatoirement (tableau d'entiers) avec une entrée et une
+     * sortie
      * avec un pourcentage d'obstacles
      */
     public void generatePlateau(int obstacle) {
+        int entrancex = 0;
+        int entrancey = 0;
+        int exit = 0;
         if (obstacle < 0 || obstacle > 100) {
             throw new IllegalArgumentException("Le pourcentage doit être compris entre 0 et 100 inclus.");
         }
         for (int x = 0; x < width; x++)
             for (int y = 0; y < height; y++)
                 maze[x][y] = random.nextInt(100) < obstacle ? 1 : 0;
-    
-        int entrancex = random.nextInt(width);
-        int entrancey = random.nextInt(height/2-1);
-        int exit = random.nextInt(width);
+
+        entrancex = random.nextInt(width);
+        entrancey = random.nextInt(height / 2 - 1);
+        exit = random.nextInt(width);
         maze[entrancex][entrancey] = 0;
         maze[exit][height - 1] = 0;
-    
+
         this.entranceCoordinate = new Coordinate(entrancex, entrancey);
         this.exitCoordinate = new Coordinate(exit, height - 1);
-        this.generatePath(entrancex, exit);
+        this.dfs(obstacle, entrancex, entrancey, exit);
+
     }
 
     private void generatePath(int x, int y) {
         int nextX, nextY;
-        List<Integer> directions = Arrays.asList(1,2,3,4);
+        List<Integer> directions = Arrays.asList(1, 2, 3, 4);
         Collections.shuffle(directions);
 
         for (int direction : directions) {
@@ -124,9 +132,43 @@ public class MazeGenerator {
             if ((nextX > 0 && nextX < width - 1) && (nextY > 0 && nextY < height - 1) && maze[nextX][nextY] == 1) {
                 maze[nextX][nextY] = 0;
                 maze[(x + nextX) / 2][(y + nextY) / 2] = 0;
-                this.generatePath(nextX, nextY);
+                if ((nextX > 1 && nextX < width - 2) && (nextY > 1 && nextY < height - 2) && maze[nextX][nextY] == 1) {
+                    this.generatePath(nextX, nextY);
+                }
             }
         }
+    }
+
+    public boolean dfs(int startX, int startY, int endX, int endY) {
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
+        Stack<int[]> stack = new Stack<>();
+        stack.push(new int[] { startX, startY });
+
+        while (!stack.isEmpty()) {
+            int[] current = stack.pop();
+            int currentX = current[0];
+            int currentY = current[1];
+
+            if (currentX < 0 || currentX >= maze.length || currentY < 0 || currentY >= maze[0].length
+                    || maze[currentX][currentY] == 1 || visited[currentX][currentY]) {
+                continue;
+            }
+
+            visited[currentX][currentY] = true;
+
+            if (currentX == endX && currentY == endY) {
+                return true;
+            } else {
+                maze[currentX][currentY] = 0;
+            }
+
+            stack.push(new int[] { currentX - 1, currentY });
+            stack.push(new int[] { currentX + 1, currentY });
+            stack.push(new int[] { currentX, currentY - 1 });
+            stack.push(new int[] { currentX, currentY + 1 });
+        }
+
+        return false;
     }
 
     public Coordinate getEntranceCoordinate() {
@@ -136,16 +178,16 @@ public class MazeGenerator {
     public Coordinate getExitCoordinate() {
         return exitCoordinate;
     }
-    
 
-    public int getEntranceRow() { 
+    public int getEntranceRow() {
         return getEntranceCoordinate().getRow();
     }
-    public int getEntranceCol() { 
+
+    public int getEntranceCol() {
         return getEntranceCoordinate().getCol();
     }
 
-    public int getExitRow() { 
+    public int getExitRow() {
         return getExitCoordinate().getRow();
     }
 
